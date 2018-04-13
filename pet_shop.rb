e4bdd377a3c4fb9b38d5eb3ba8c64e79f9f40e15
rurigrass <ruripg@gmail.com>
2018-04-13 11:45:25 +0100

    part 4 done

--- pet_shop.rb
+++ pet_shop.rb
@@ -4,8 +4,13 @@ def pet_shop_name(shop)
 end
 
 #2
-def total_cash(shop)
-  return shop[:admin][:total_cash]
+def total_cash(cash)
+  return cash[:admin][:total_cash]
 end
 
 #3
+def add_or_remove_cash(cash, add)
+  return cash[:admin][:total_cash] += add.to_i
+end
+
+#4
